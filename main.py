324a6b103491c0a9d99c564ff1217fada9fafdd0
ISchaste <daniilb7@mail.ru>
2024-11-19 03:42:41 +0500

    add class Order

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
 from fastapi import FastAPI
+import datetime
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class Order(BaseModel):
+    number : int
+    startDate: datetime.date
+    device : str
+    problemType: str
+    description: str
+    client: str
+    status : str
+
 
 @app.get("/")
 def read_root():
